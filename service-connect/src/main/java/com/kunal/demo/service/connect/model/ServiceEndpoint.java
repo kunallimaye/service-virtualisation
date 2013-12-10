b4560c5962a91579461483c8639237a5beb4524c
Kunal Limaye <kunallimaye@gmail.com>
2013-12-10 22:30:40 +1100

    Adding service-connect project to github. Parent project compiles with no errors.

---
+++ service-connect/src/main/java/com/kunal/demo/service/connect/model/ServiceEndpoint.java
@@ -0,0 +1,131 @@
+package com.kunal.demo.service.connect.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+
+@Entity
+public class ServiceEndpoint implements Serializable
+{
+
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = -5190387346243918296L;
+@Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id = null;
+   @Version
+   @Column(name = "version")
+   private int version = 0;
+
+   @Column(unique=true)
+   private String name;
+
+   @Column
+   private String description;
+
+   @Column
+   private String url;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object that)
+   {
+      if (this == that)
+      {
+         return true;
+      }
+      if (that == null)
+      {
+         return false;
+      }
+      if (getClass() != that.getClass())
+      {
+         return false;
+      }
+      if (id != null)
+      {
+         return id.equals(((ServiceEndpoint) that).id);
+      }
+      return super.equals(that);
+   }
+
+   @Override
+   public int hashCode()
+   {
+      if (id != null)
+      {
+         return id.hashCode();
+      }
+      return super.hashCode();
+   }
+
+   public String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(final String name)
+   {
+      this.name = name;
+   }
+
+   public String getDescription()
+   {
+      return this.description;
+   }
+
+   public void setDescription(final String description)
+   {
+      this.description = description;
+   }
+
+   public String getUrl()
+   {
+      return this.url;
+   }
+
+   public void setUrl(final String url)
+   {
+      this.url = url;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (name != null && !name.trim().isEmpty())
+         result += "name: " + name;
+      if (description != null && !description.trim().isEmpty())
+         result += ", description: " + description;
+      if (url != null && !url.trim().isEmpty())
+         result += ", url: " + url;
+      return result;
+   }
+}
